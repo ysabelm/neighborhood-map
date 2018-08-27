3db5ebee1a9e83ca9f37f3e1d3f9ec49c256b426
ysabelm <ysabelmonfort@gmail.com>
2018-08-27 18:25:26 +0200

    Changes in Aside Menu and adds Foursquare credentials

--- src/components/AsideMenu/AsideMenu.js
+++ src/components/AsideMenu/AsideMenu.js
@@ -3,13 +3,29 @@ import './AsideMenu.css'
 
 class AsideMenu extends Component {
 
-    render() {
+  render() {
 
-        return (
-            <aside>
-            </aside>
-        )
-    }
+    return (
+
+      <aside>
+        <ul id="my-favorite-places">
+          <li><a href="#">Vin Au Verre</a></li>
+          <li><a href="#">A La Bière Comme A La Bière</a></li>
+          <li><a href="#">La Cave des Abbesses</a></li>
+          <li><a href="#">De Verre en Vers</a></li>
+          <li><a href="#">Paris Saint-Bière</a></li>
+        </ul>
+        <div className="search-form">
+          <label htmlFor="searchQuery">Find Another Place</label>
+          <input
+            id="searchQuery"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+      </aside>
+    )
+  }
 }
 
 export default AsideMenu
