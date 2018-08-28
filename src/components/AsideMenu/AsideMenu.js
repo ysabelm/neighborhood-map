9e4fe17fa2e496a86ebfe42df477a13bf142e36f
ysabelm <ysabelmonfort@gmail.com>
2018-08-28 21:05:52 +0200

    Changes in query

--- src/components/AsideMenu/AsideMenu.js
+++ src/components/AsideMenu/AsideMenu.js
@@ -9,21 +9,21 @@ class AsideMenu extends Component {
 
       <div className="aside-menu">
         <div className="my-favorite-places">
-          <h2 aria-level="2">My favorite places</h2>
+          <h2 aria-level="2">My Favorite Theatres</h2>
           <ul id="list-favorite-places" aria-label="List favorite places">
-            <li><a role="menuitem" href="#">Vin Au Verre</a></li>
-            <li><a role="menuitem" href="#">A La Bière Comme A La Bière</a></li>
-            <li><a role="menuitem" href="#">La Cave des Abbesses</a></li>
-            <li><a role="menuitem" href="#">De Verre en Vers</a></li>
-            <li><a role="menuitem" href="#">Paris Saint-Bière</a></li>
+            <li><a role="menuitem" href="#">Le Monfort Théâtre</a></li>
+            <li><a role="menuitem" href="#">Théâtre Gérard Philippe</a></li>
+            <li><a role="menuitem" href="#">Les Bouffes du Nord</a></li>
+            <li><a role="menuitem" href="#">Comédie-Française</a></li>
+            <li><a role="menuitem" href="#">Théâtre de la Ville</a></li>
           </ul>
         </div>
         <div className="search-form" aria-label="Search place">
-          <label>Find Your Place</label>
+          <label>Find Your Theatre</label>
           <input
             id="searchPlace"
             type="text"
-            placeholder="Enter your category..."
+            placeholder="Enter here..."
           />
         </div>
       </div>
