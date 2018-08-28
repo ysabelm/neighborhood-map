c277db08ec9c73c68e79a653cc6bd1e304b7ae40
ysabelm <ysabelmonfort@gmail.com>
2018-08-28 19:01:09 +0200

    Get places with id from foursquare

--- src/fourSquareAPI.js
+++ src/fourSquareAPI.js
@@ -1,27 +1,46 @@
 import { CLIENT_ID, CLIENT_SECRET } from './data/credentials';
 
-const CLIENT_ID = {CLIENT_ID};
-const CLIENT_SECRET = {CLIENT_SECRET};
-const API = "https://api.foursquare.com/v2";
+const API = "https://api.foursquare.com/v2/";
 const VERSION = "20180828";
 
-const RADIUS = 250;
-const SEARCH_RESULTS = 1;
-// Add a category parameter
-const CATEGORIES = {}
+const RADIUS = 500;
+const SEARCH_RESULTS = 25;
+const CATEGORIES = {
+  wine_bar: '4bf58dd8d48988d123941735',
+  wine_shop: '4bf58dd8d48988d119951735',
+  beer_store: '5370f356bcbc57f1066c94c2',
+  beer_bar: '56aa371ce4b08b9a8d57356c'
+}
 
-// Return a venue id from FourSquare including lat, lng & name.
+const CATEGORY_ID = Object.keys(CATEGORIES).map((cat) => CATEGORIES[cat]);
 
-export const getSearchResult = (lat, lng, name) =>
-	fetch(`${API}/venues/search?ll=${lat},${lng}&limit=${SEARCH_RESULTS}&radius=${RADIUS}&query=${name}
-    	&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}`)
-	  	.then(response=>response.json())
-	  	.then(response=>response.response.venues[0].id)
-	  	.catch('error')
+// Return venues url from FourSquare.
+export const getFsLocations = (mapCenter) => {
 
-//Return an array of details about a place from FourSquare. Takes venue id.
+  const requestURL = `${API}/venues/search?ll=${mapCenter.lat},${mapCenter.lng}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}&categoryId=${CATEGORY_ID}&radius=${RADIUS}&limit=${SEARCH_RESULTS}`
+  return fetch(requestURL)
+    .then(response => {
+      if (!response.ok) {
+        throw response
+      } else return response.json()
+    })
+    .then(data => {
+      const places = data.response.venues;
+      const filteredPlaces = places.filter(place => place.location.address && place.location.city && place.location.city === "Paris");
 
-export const getDetails = (id) =>
-	fetch(`${API}/venues/${id}?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}`)
-  		.then(res => res.json())
-  		.catch('error')
+      return filteredPlaces;
+    })
+}
+
+// Return an array of details about a venue. It uses id.
+export const getFsDetails = (fsid) => {
+  const FSID =  fsid;
+
+  const requestURL = `${API}/venues/${FSID}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${VERSION}`
+  return  fetch(requestURL)
+  .then(response => {
+      if (!response.ok) {
+        throw response
+      } else  return response.json()
+    })
+}
