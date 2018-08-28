a81b4d6f696a574a1cb55a6ad4264baeb1390300
ysabelm <ysabelmonfort@gmail.com>
2018-08-28 22:00:47 +0200

    Displays markers and modals

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import scriptLoader from 'react-async-script-loader';
-import { MAP_KEY } from './data/credentials';
 import { mapStyles } from './data/mapStyles.js';
 import './App.css';
 
@@ -8,108 +6,89 @@ import Map from './components/Map/Map'
 import Header from './components/Header/Header'
 import AsideMenu from './components/AsideMenu/AsideMenu'
 import { pointsOfInterest } from './data/poi.js'
-import { places, filteredPlaces } from './fourSquareAPI'
 
 
 class App extends Component {
 
+
   state = {
-    map: {},
-    mapCenter: {
-      lat: 48.855215,
-      lng: 2.347154
-    },
-    markers: [],
-    places: [],
-    filteredPlaces: [],
-    infoWindow: {},
-    mapReady: false,
-    mapError: false
+    venues: []
   }
 
-  componentWillReceiveProps({ isScriptLoadSucceed }) {
-    if (isScriptLoadSucceed && !this.state.mapReady) {
+  componentDidMount() {
+    this.getVenues()
+  }
 
-      // Create a new map
-      const map = new window.google.maps.Map(document.getElementById('map'), {
-        zoom: 13,
-        center: this.state.mapCenter,
-        styles: mapStyles
-      });
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDUjgOkGFM4wo-D5tiFSihOZnJwyk4M8zg&callback=initMap")
+    window.initMap = this.initMap
+  }
 
-      const infoWindow = new window.google.maps.InfoWindow();
-
-      this.setState({
-        map: map,
-        infoWindow: infoWindow,
-        mapReady: true
-      })
-    } else if (!this.state.mapReady) {
-      console.log("The map couldn't load");
-      this.setState({ mapError: true })
+  getVenues = () => {
+    const venuesEndPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "30HUM4RXCRADFSCC1NFO4YQWL54T41NKHR5SN43LKTHGAWI5",
+      client_secret: "W0OSXW2J3RLORN5VFGGQR2Q0FJWTPJCK1QGUH5MMXBLOUI53",
+      query: "théâtre",
+      near: "Paris",
+      v: "20182008"
     }
-    // Create an array of markers using the pointsOfInterest
-    for (let i = 0; i < pointsOfInterest.length; i++) {
-      // Get the position from the location array.
-      let position = pointsOfInterest[i].location;
-      let title = pointsOfInterest[i].title;
-      // Create a marker per location, and put into markers array.
-      let marker = new window.google.maps.Marker({
-        position: position,
-        title: title,
-        animation: window.google.maps.Animation.DROP,
-        id: i
-      });
-      // Push the marker to our array of markers.
-      this.state.markers.push(marker);
-    }
-    this.state.places.map((place) => {
 
-      // Create Markers
-      let marker = new window.google.maps.Marker({
-        position: { lat: place.venue.location.lat, lng: place.venue.location.lng },
-        map: this.state.map,
-        animation: window.google.maps.Animation.DROP,
-        title: place.venue.name
+
+
+    fetch(venuesEndPoint + new URLSearchParams(parameters), {
+      method: 'GET'
+    })
+      .then(response => response.json()).then(response => {
+        this.setState({
+          venues: response.response.groups[0].items
+        }, this.renderMap())
+
+      })
+      .catch(error => {
+        console.log("fetch error" + error)
       })
 
-      // Add each created marker to the 'markers' array
-      this.state.markers.push(marker)
+  }
 
-      // Create InfoWindow
-      let content = `
-                      <h1>${place.venue.name}</h1>
-                      <p>Address: ${place.venue.location.formattedAddress[0]} ${place.venue.location.formattedAddress[1]} ${place.venue.location.formattedAddress[2]}</p>
-                      <p>lat: ${place.venue.location.lat}, long: ${place.venue.location.lng}</p>
-                      `
+  initMap = () => {
 
-      // Display the InfoWindow after clicking on the Marker
-      marker.addListener('click', function () {
-
-        // Update 'InfoWindow' content
-        this.state.infowindow.setContent(content)
-
-        // Open An 'InfoWindow'
-        this.state.infowindow.open(this.state.map, marker)
-
-        // Animate The Marker
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(window.google.maps.Animation.BOUNCE);
-        }
-      })
+    // Create A Map
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 48.855215, lng: 2.347154 },
+      zoom: 13,
+      styles: mapStyles
     })
 
-    
+    // Create An InfoWindow
+    const infowindow = new window.google.maps.InfoWindow({maxWidth: 300})
+
+    // Display Dynamic Markers
+    this.state.venues.map(myVenue => {
+
+      const contentString = `${myVenue.venue.name}
+      ${myVenue.venue.location.formattedAddress[0]}
+      ${myVenue.venue.location.formattedAddress[1]}`
+
+      // Create A Marker
+      var marker = new window.google.maps.Marker({
+        position: { lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng },
+        map: map,
+        title: myVenue.venue.name,
+      })
+
+      // Click on A Marker!
+      marker.addListener('click', function () {
+
+        // Change the content
+        infowindow.setContent(contentString)
+
+        // Open An InfoWindow
+        infowindow.open(map, marker)
+      })
+
+    })
   }
-
-
-
-
-
-
-
 
   render() {
     return (
@@ -124,7 +103,13 @@ class App extends Component {
   }
 }
 
-// Load Googgle Map asynchronously
-export default scriptLoader(
-  [`https://maps.googleapis.com/maps/api/js?key=${MAP_KEY}&libraries:places`]
-)(App);
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+export default App;
