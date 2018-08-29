565abcc60e12a6f1ec66926f532885016cb53866
ysabelm <ysabelmonfort@gmail.com>
2018-08-29 10:59:33 +0200

    Adds searchVenue function - Completes input in search-form - Changes in my-favorite-places

--- src/components/AsideMenu/AsideMenu.js
+++ src/components/AsideMenu/AsideMenu.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react'
 import './AsideMenu.css'
+import escapeRegExp from 'escape-string-regexp'
 
 class AsideMenu extends Component {
+
+  state = {
+    query: '',
+    venues: this.props.venues
+  }
+
+  searchVenue = (query) => {
+    this.setState({ query })
+    let searchResult
+    let venues = this.props.venues
+
+    if (this.state.query && (this.state.query !== '')) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      searchResult = venues.filter(venue => match.test(venue.venue.name))
+      this.setState({ venues: searchResult })
+      this.props.updatePlaces(searchResult)
+    } else {
+      this.setState({ venues: venues })
+    }
+  }
+
+  handleOnClickMarker = (venueTitle) => {
+    this.props.markers.map((marker) => {
+      if (marker.title === venueTitle) {
+        window.google.maps.event.trigger(marker, 'click');
+      }
+    })
+  }
 
   render() {
 
     return (
 
       <div className="aside-menu">
-        <div className="my-favorite-places">
-          <h2 aria-level="2">My Favorite Theatres</h2>
-          <ul id="list-favorite-places" aria-label="List favorite places">
-            <li><a role="menuitem" href="#">Le Monfort Théâtre</a></li>
-            <li><a role="menuitem" href="#">Théâtre Gérard Philippe</a></li>
-            <li><a role="menuitem" href="#">Les Bouffes du Nord</a></li>
-            <li><a role="menuitem" href="#">Comédie-Française</a></li>
-            <li><a role="menuitem" href="#">Théâtre de la Ville</a></li>
-          </ul>
-        </div>
-        <div className="search-form" aria-label="Search place">
+        <div className="search-form" aria-label="Search theatre">
           <label>Find Your Theatre</label>
           <input
-            id="searchQuery"
+            id="searchPlace"
             type="text"
-            placeholder="Enter your query..."
+            placeholder="Search a place..."
+            onChange={event => this.searchVenue(event.target.value)}
+            value={this.state.query}
           />
+        </div>
+        <div className="my-favorite-places">
+          <h2 aria-level="2">My favorite places</h2>
+          {this.state.venues.length !== 0 && (
+            <ul id="list-favorite-places" aria-label="List favorite places">
+              {this.state.venues.map((venue, index) => (
+                <li
+                  key={index}
+                  tabIndex={index}
+                  className="item"
+                  onClick={() => this.handleOnClickMarker(venue.venue.name)}
+                >
+                  {venue.venue.name}
+                </li>
+              ))}
+            </ul>
+          )}
+          {this.state.venues === 0 && (
+            <p>No Places Found...</p>
+          )}
         </div>
       </div>
     )
